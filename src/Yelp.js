69e5fea11aca5a1572ace6f47cb5e001a0fc5c7c
marcburnett <marc_burnett@yahoo.com>
2022-04-06 13:27:27 +0100

    update Yelp url

--- src/Yelp.js
+++ src/Yelp.js
@@ -1,5 +1,5 @@
 const apikey = 'SE1UH3C-KigK2VRZ-9j4E23WscctPZMIrMSEXmG5PXYHKFn53mnxTwwzuNi0jQqvjoakeyoYF0vHcuyZq-u1SFznmX8rL27Y4PHYW5uXicSM-cgDPI5wvIyGLKpMYnYx'
-
+const url ='https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sortBy=${sortBy}'
 
 export let Yelp = {
 	
@@ -7,7 +7,7 @@ export let Yelp = {
   search(term , location, sortBy)
   {
 
-	return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sortBy=${sortBy}`,
+	return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sortBy=${sortBy}`,
 	
 			{headers: {Authorization: `Bearer ${apikey}`}
 
